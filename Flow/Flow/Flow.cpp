86e48441a42218ffcf25d344ca01d33b14a0426e
Kevin Waki <waki@xetova.com>
2020-04-09 23:09:56 +0300

    Using a while loop to control number of times

--- Flow/Flow/Flow.cpp
+++ Flow/Flow/Flow.cpp
@@ -6,19 +6,28 @@ int main() {
 
 	//declare variables
 	int i, j;
+	bool keepGoing = true;
 
-	//get user input
-	cout << "Enter a number:  ";
-	cin >> i;
-	cout << "You entered : " << i << ". Enter another number: ";
-	cin >> j;
+	//use while loop to control when to stop comparing numbers
+	while (keepGoing) {
 
-	//use if statement to control display
-	if (i < j) {
-		cout << "The first number ," << i << " is less than the second number, " << j << endl;
-	}
-	else {
-		cout << "The first number ," << i << " is greater than the second number, " << j << endl;
+		//get user input
+		cout << "Enter a number:  ";
+		cin >> i;
+		cout << "You entered : " << i << ". Enter another number: ";
+		cin >> j;
+
+		//use if statement to control display
+		if (i < j) {
+			cout << "The first number ," << i << " is less than the second number, " << j << endl;
+		}
+		else {
+			cout << "The first number ," << i << " is greater than the second number, " << j << endl;
+		}
+
+		//ask the use if the want to keep going.
+		cout << "Do you want to run this again ? ";
+		cin >> keepGoing;
 	}
 	return 0;
 }
