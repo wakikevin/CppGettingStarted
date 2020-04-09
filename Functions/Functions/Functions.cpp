9ed01419bd4942c2d9f8a70e72a2cbf304f13fe1
Kevin Waki <waki@xetova.com>
2020-04-10 00:11:52 +0300

    Function overloading - two functions same name

--- Functions/Functions/Functions.cpp
+++ Functions/Functions/Functions.cpp
@@ -6,11 +6,19 @@ int add(int x, int y) {
 	return x + y; //return the sum
 }
 
+//overloading
+int add(int x, int y, int z) {
+	return add(x,y)+z; //return the sum
+}
+
 //the main function
 int main() {
 
 	//declare a variable and initialize with calling the defined function
 	int sum = add(3, 7);
 	cout << "3+7 is: " << sum << endl;
+
+	int b = add(3, 7,2);
+	cout << "3+7+2 is: " << b << endl;
 	return 0;
 }
