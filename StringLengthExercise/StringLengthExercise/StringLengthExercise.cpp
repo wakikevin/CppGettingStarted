c505c4796eeccefba8d84927915eaf0daa7b99b5
Kevin Waki <waki@xetova.com>
2020-04-13 18:02:42 +0300

    Test to check which string is longer

---
+++ StringLengthExercise/StringLengthExercise/StringLengthExercise.cpp
@@ -0,0 +1,30 @@
+#include <iostream> //include the iostream library
+#include <string> //include the string library
+
+using namespace std; //use the standard namespace
+
+//main funtion
+int main() {
+	//declare the variables
+	string strOne, strTwo;
+	int strOneLen, strTwoLen;
+
+	//ask for the user input
+	cout << "Enterthe first string: ";
+	cin >> strOne;
+	cout << "Enter the second string: ";
+	cin >> strTwo;
+
+	//get the length of the strings
+	strOneLen = strOne.length();
+	strTwoLen = strTwo.length();
+
+	//comapre the lenths
+	if (strOneLen > strTwoLen) {
+		cout << "The first string " << "\"" + strOne + "\"" << " is longer.";
+	}
+	else {
+		cout << "The second string " << "\"" + strTwo + "\"" << " is longer.";
+	}
+	return 0;
+}
